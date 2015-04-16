cbcfe87b6a99eeeff0911d14c29c3d2011a06cd8
bgrimes78 <brandon.grimes@willowtreeapps.com>
2015-04-16 11:57:05 -0400

    fix it

--- spec/Example_rspec-it_spec.rb
+++ spec/Example_rspec-it_spec.rb
@@ -6,11 +6,11 @@ describe "Example" do
   let(:ex) { Example.new(@Capybara) }
 
   it 'example' do 
-    @ex.exmaple_1
-    @ex.exmaple_2
-    @ex.exmaple_3
-    @ex.exmaple_4
-    @ex.exmaple_5
+    ex.exmaple_1
+    ex.exmaple_2
+    ex.exmaple_3
+    ex.exmaple_4
+    ex.exmaple_5
   end
 
 end
